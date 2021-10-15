fd117cdce164f3324ba838bc6e06893fb7c14c19
Oneplate <jgbjisoo@gmail.com>
2021-10-15 23:34:23 +0900

    Course 01 update

---
+++ Exercises1_if.py
@@ -0,0 +1,24 @@
+
+while True:
+    print ("#####################################################")
+    print ("# This is loop for comparison between three numbers #")
+    print ("#  type a for positivity test and b for comparison  #")
+    print ("#               also, type q for quit               #")
+    print ("#####################################################")
+    switch = input("Type a character: ")
+    x = input("Now, type the first number you want to compare.: ")
+    y = input("Second number.: ")
+    z = input("Last number.: ")
+    x = int(x)
+    y = int(y)
+    z = int(z)
+    
+    if switch == 'a':
+        
+    elif switch == 'b':
+
+    elif switch == 'q':
+        break
+    else:
+        continue
+    
