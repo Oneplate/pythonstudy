02cfa9c67de5973c3cca7c4415228876956ed8d5
Oneplate <jgbjisoo@gmail.com>
2021-10-16 22:57:56 +0900

    minor modification

--- Exercises1_if.py
+++ Exercises1_if.py
@@ -6,19 +6,34 @@ while True:
     print ("#               also, type q for quit               #")
     print ("#####################################################")
     switch = input("Type a character: ")
-    x = input("Now, type the first number you want to compare.: ")
-    y = input("Second number.: ")
-    z = input("Last number.: ")
+    if switch == 'q':
+        break
+    
+    x = input("Now, type the first number, x, you want to compare.: ")
+    y = input("Second number, y.: ")
+    z = input("Last number, z.: ")
     x = int(x)
     y = int(y)
     z = int(z)
     
     if switch == 'a':
-        
+        if x > 0:
+            print ("x is positive")
+        if y > 0:
+            print ("y is positive")
+        else:
+            print ("y is not positive")
+        if z > 0:
+            print ("z is positive")
+        elif z < 0:
+            print("z is negative")
+        else:
+            print("z must be 0")
     elif switch == 'b':
-
-    elif switch == 'q':
-        break
+        print("x is equal to y: ", x == y)
+        print("x is not equal to y: ", x != y)
+        print("x is equal to z: ", x == z)
+        print("x is not equal to z: ", x != z)
     else:
         continue
     
